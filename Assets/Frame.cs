d552c29327cb8a02a701b8ef20159e9a297adefc
LAPTOP-KAR0J5S8\suginomasashi <sgnmssuioaai@gmail.com>
2019-09-08 01:16:02 +0900

    いろいろ

---
+++ Assets/Frame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frame 
+{
+
+    public float latitude;
+    public float longitude;
+    public float altitude;
+    public float anglebasednorth;
+    public Vector3 acceleration;
+    public bool isactive;
+    public string description;
+}
