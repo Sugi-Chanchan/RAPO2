380f0a41e1ae49bc1efb0fa845a57ff1a15c65aa
LAPTOP-KAR0J5S8\suginomasashi <sgnmssuioaai@gmail.com>
2019-09-08 00:47:31 +0900

    いろいろ

--- Assets/EditorPasyari.cs
+++ Assets/EditorPasyari.cs
@@ -47,8 +47,11 @@ public class EditorPasyari : MonoBehaviour
         yield return new WaitForEndOfFrame();
 
         photo.SetPixels(webcamtexture.GetPixels());
+
+
         photo.Apply();
 
+        photo = rotateTexture(photo,true);
 
         //Encode to a PNG
         byte[] bytes = photo.EncodeToPNG();
@@ -57,5 +60,30 @@ public class EditorPasyari : MonoBehaviour
 
 
 
+    }
+
+    Texture2D rotateTexture(Texture2D originalTexture, bool clockwise)
+    {
+        Color32[] original = originalTexture.GetPixels32();
+        Color32[] rotated = new Color32[original.Length];
+        int w = originalTexture.width;
+        int h = originalTexture.height;
+
+        int iRotated, iOriginal;
+
+        for (int j = 0; j < h; ++j)
+        {
+            for (int i = 0; i < w; ++i)
+            {
+                iRotated = (i + 1) * h - j - 1;
+                iOriginal = clockwise ? original.Length - 1 - (j * w + i) : j * w + i;
+                rotated[iRotated] = original[iOriginal];
+            }
+        }
+
+        Texture2D rotatedTexture = new Texture2D(h, w);
+        rotatedTexture.SetPixels32(rotated);
+        rotatedTexture.Apply();
+        return rotatedTexture;
     }
 }
--- Assets/takePhoto.cs
+++ Assets/takePhoto.cs
@@ -44,24 +44,21 @@ public class takePhoto : MonoBehaviour
         // be sure to scroll down to the SECOND long example on that doco page 
 
         Texture2D photo = new Texture2D(webcamtexture.width, webcamtexture.height);
-        photo = RotateTexture.rotate(photo);
-        
+
 
         yield return new WaitForEndOfFrame();
 
         photo.SetPixels(webcamtexture.GetPixels());
+
+        photo = rotateTexture(photo, true);
         photo.Apply();
 
-
-        //Encode to a PNG
-        byte[] bytes = photo.EncodeToPNG();
-        //Write out the PNG. Of course you have to substitute your_path for something sensible
-        File.WriteAllBytes(Application.dataPath + "photo.png", bytes);
+        
 
 
 
 
-
+        NativeGallery.SaveImageToGallery(photo, "GalleryTest", "My img {0}.png");
 
 
 
@@ -109,5 +106,30 @@ public class takePhoto : MonoBehaviour
         //   NativeGallery.SaveImageToGallery(photo, "rapo", "My img {10}.jpg");
     }
 
-    
+
+    Texture2D rotateTexture(Texture2D originalTexture, bool clockwise)
+    {
+        Color32[] original = originalTexture.GetPixels32();
+        Color32[] rotated = new Color32[original.Length];
+        int w = originalTexture.width;
+        int h = originalTexture.height;
+
+        int iRotated, iOriginal;
+
+        for (int j = 0; j < h; ++j)
+        {
+            for (int i = 0; i < w; ++i)
+            {
+                iRotated = (i + 1) * h - j - 1;
+                iOriginal = clockwise ? original.Length - 1 - (j * w + i) : j * w + i;
+                rotated[iRotated] = original[iOriginal];
+            }
+        }
+
+        Texture2D rotatedTexture = new Texture2D(h, w);
+        rotatedTexture.SetPixels32(rotated);
+        rotatedTexture.Apply();
+        return rotatedTexture;
+    }
+
 }
