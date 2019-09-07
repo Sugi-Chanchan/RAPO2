41a7024b23efaeff42ded3e0becb7664ef57e9f9
watson70percent <38928314+watson70percent@users.noreply.github.com>
2019-09-08 07:38:00 +0900

    画像と文字ついか

--- Assets/mapMaking.cs
+++ Assets/mapMaking.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class mapMaking : MonoBehaviour
 {
@@ -14,18 +16,28 @@ public class mapMaking : MonoBehaviour
     public Frame Frame;
     public GameObject sp;
     Sprite sprite;
+    Image pic;
+
+    Text message;
+    public Text text;
+
+
+    public Image image;
+
+    public Canvas canvas;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        sprite = sp.GetComponent<Sprite>();
+        sprite = sp.GetComponent<SpriteRenderer>().sprite;
 
         List<Frame> frames = new List<Frame>();
-        frames.Add(new Frame(2, 3, 2, 0, new Vector3(0, 0, 0), true, "", sprite));
-        frames.Add(new Frame(3, 3, 2, 0, new Vector3(0, 0, 0), true, "", sprite));
-        frames.Add(new Frame(4, 3, 2, 0, new Vector3(0, 0, 0), true, "", sprite));
-        frames.Add(new Frame(4, 2, 2, 0, new Vector3(0, 0, 0), true, "", sprite));
-        frames.Add(new Frame(4, 1, 2, 0, new Vector3(0, 0, 0), true, "", sprite));
+        frames.Add(new Frame(2, 3, 2, 0, new Vector3(0, 0, 0), true, "スタート", sprite));
+        frames.Add(new Frame(3, 3, 2, 0, new Vector3(0, 0, 0), false, "", sprite));
+        frames.Add(new Frame(4, 3, 2, 0, new Vector3(0, 0, 0), true, "左", sprite));
+        frames.Add(new Frame(4, 2, 2, 0, new Vector3(0, 0, 0), false, "", sprite));
+        frames.Add(new Frame(4, 1, 2, 0, new Vector3(0, 0, 0), true, "ゴール", sprite));
         lineRenderer = LINE.GetComponent<LineRenderer>();
 
         makeMap(frames);
@@ -46,6 +58,12 @@ public class mapMaking : MonoBehaviour
             if (frames[i].isactive)
             {
                 Instantiate(ball, vector3, Quaternion.identity);
+                pic = Instantiate(image, new Vector3(i, 0, 0), Quaternion.identity);
+                pic.GetComponent<Image>().sprite = frames[i].sprite;
+                pic.transform.SetParent(canvas.transform);
+                message = Instantiate(text, new Vector3(i, 4, 4), Quaternion.identity);
+                message.GetComponent<Text>().text = frames[i].description;
+                message.transform.SetParent(canvas.transform);
 
             }
  
